348adf5d0f0b753313ea27ffdcad31201a6196ab
wzb <864457005@qq.com>
2021-08-02 16:15:01 +0800

    Improve rotate

--- src/rotate.js
+++ src/rotate.js
@@ -7,8 +7,6 @@ function rotate(imgVer,angle) {
     let ih = imgVer.length;
     let iw = imgVer[0].length;
 
-    let ow = ih;
-    let oh = iw;
 
     let rad = angle * Math.PI/180;
     let rotateVer = [
@@ -18,31 +16,32 @@ function rotate(imgVer,angle) {
     ];
     let ori = [
         1,0,0,
-        0,-1,0,
-        -0.5*(iw-1),0.5*(ih-1),1
+        0,1,0,
+        -0.5*(iw-1),-0.5*(ih-1),1
     ];
 
     let dec = [
         1,0,0,
-        0,-1,0,
+        0,1,0,
         0.5*(iw-1),0.5*(ih-1),1
     ];
 
-    for(let r=0;r<oh;r++){
-        dist.push([]);
-    }
+
+    let a1 = micMul(ori,rotateVer);
+    let m1 = micMul(a1,dec);
+
+
 
     for(let r=0;r<ih;r++){
         for(let c=0;c<iw;c++){
 
-            let result = verMul([r,c,1],ori);
-            result = verMul(result,rotateVer);
-            result = verMul(result,dec);
+            let r2 = verMul([r,c,1],m1);
 
-            let x = Math.round(result[0]);
-            let y = Math.round(result[1]);
+            let x = (r2[0]);
+            let y = (r2[1]);
 
-            dist[x][y] =  imgVer[r][c]
+            dist[x] = dist[x] || []
+            dist[x][y] = imgVer[r][c]
         }
     }
     return dist;
@@ -53,12 +52,22 @@ function verMul(a,b){
     let r2= a[0]*b[1] + a[1]*b[4] + a[2]*b[7];
     let r3= a[0]*b[2] + a[1]*b[5] + a[2]*b[8];
 
-
-    return [(r1),(r2),(r3)];
+    return [Math.round(r1),Math.round(r2),Math.round(r3)];
 }
 
+function micMul(a,b){
+    return [
+        ...verMul([a[0],a[1],a[2]],b),
+        ...verMul([a[3],a[4],a[5]],b),
+        ...verMul([a[6],a[7],a[8]],b),
+    ]
+}
+
+
 console.log(rotate([
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-],180))
+    [0,0,0,0,0],
+    [0,1,2,3,0],
+    [0,4,5,6,0],
+    [0,7,8,9,0],
+    [0,0,0,0,0]
+],-90))
